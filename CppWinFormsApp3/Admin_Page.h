348407a7f827b0a35b9e65b850d9eac8a6fe9b7d
zzeyadnader <zeyadnader00@gmail.com>
2025-12-14 19:20:07 +0200

    Solve problems in code

--- CppWinFormsApp3/Admin_Page.h
+++ CppWinFormsApp3/Admin_Page.h
@@ -1,190 +1,148 @@
 #pragma once
-#include<Windows.h>
+#include <Windows.h>
+
 namespace CppWinFormsApp3 {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
+    using namespace System::Drawing::Drawing2D;
 
+    public ref class Admin_Page : public System::Windows::Forms::Form
+    {
+    public:
+        Admin_Page(void)
+        {
+            InitializeComponent();
 
-	public ref class Admin_Page : public System::Windows::Forms::Form
-	{
-	public:
-		Admin_Page(void)
-		{
-			InitializeComponent();
+            // Round buttons AFTER InitializeComponent
+            RoundButton(btnStudents, 30);
+            RoundButton(btnDepartments, 30);
+            RoundButton(btnResult, 30);
+        }
 
-			RoundButton(btnStudents, 30);
-			RoundButton(btnDepartments, 30);
-			RoundButton(btnResult, 30);
-		}
+    protected:
+        ~Admin_Page()
+        {
+            if (components)
+                delete components;
+        }
 
-	protected:
+    private:
+        // ===== Controls =====
+        Panel^ panel1;
+        Label^ lblTitle;
+        PictureBox^ pictureBoxLogo;
 
-		~Admin_Page()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
-	private: System::Windows::Forms::Panel^ panel1;
-	private: System::Windows::Forms::Label^ lblTitle;
-	private: System::Windows::Forms::PictureBox^ pictureBoxLogo;
-	private: System::Windows::Forms::Button^ button3;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::Button^ button2;
+        Button^ btnStudents;
+        Button^ btnDepartments;
+        Button^ btnResult;
+        Button^ buttonBack;
 
+        System::ComponentModel::Container^ components;
 
-	protected:
+        // ===== Rounded Button Function =====
+    private:
+        void RoundButton(Button^ btn, int radius)
+        {
+            if (btn == nullptr) return;
 
-	private:
-		void RoundButton(System::Windows::Forms::Button^ btn, int radius)
-		{
-			System::Drawing::Drawing2D::GraphicsPath^ path =
-				gcnew System::Drawing::Drawing2D::GraphicsPath();
+            GraphicsPath^ path = gcnew GraphicsPath();
+            int d = radius * 2;
 
-			path->AddArc(0, 0, radius, radius, 180, 90);
-			path->AddArc(btn->Width - radius, 0, radius, radius, 270, 90);
-			path->AddArc(btn->Width - radius, btn->Height - radius, radius, radius, 0, 90);
-			path->AddArc(0, btn->Height - radius, radius, radius, 90, 90);
-			path->CloseAllFigures();
+            path->AddArc(0, 0, d, d, 180, 90);
+            path->AddArc(btn->Width - d, 0, d, d, 270, 90);
+            path->AddArc(btn->Width - d, btn->Height - d, d, d, 0, 90);
+            path->AddArc(0, btn->Height - d, d, d, 90, 90);
+            path->CloseFigure();
 
-			btn->Region = gcnew System::Drawing::Region(path);
-		}
-		System::ComponentModel::Container ^components;
+            btn->Region = gcnew System::Drawing::Region(path);
+        }
 
 #pragma region Windows Form Designer generated code
-		void InitializeComponent(void)
-		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Admin_Page::typeid));
-			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->pictureBoxLogo = (gcnew System::Windows::Forms::PictureBox());
-			this->lblTitle = (gcnew System::Windows::Forms::Label());
-			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->panel1->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxLogo))->BeginInit();
-			this->SuspendLayout();
-			this->panel1->AccessibleName = L"panelHeader";
-			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(83)), static_cast<System::Int32>(static_cast<System::Byte>(39)),
-				static_cast<System::Int32>(static_cast<System::Byte>(112)));
-			this->panel1->Controls->Add(this->pictureBoxLogo);
-			this->panel1->Controls->Add(this->lblTitle);
-			this->panel1->Location = System::Drawing::Point(96, 12);
-			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(1156, 116);
-			this->panel1->TabIndex = 0;
-			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Admin_Page::panel1_Paint);
-			this->pictureBoxLogo->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
-			this->pictureBoxLogo->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBoxLogo.Image")));
-			this->pictureBoxLogo->Location = System::Drawing::Point(1035, 7);
-			this->pictureBoxLogo->Name = L"pictureBoxLogo";
-			this->pictureBoxLogo->Size = System::Drawing::Size(101, 98);
-			this->pictureBoxLogo->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
-			this->pictureBoxLogo->TabIndex = 1;
-			this->pictureBoxLogo->TabStop = false;
-			this->lblTitle->AccessibleDescription = L"";
-			this->lblTitle->AccessibleName = L"";
-			this->lblTitle->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->lblTitle->Font = (gcnew System::Drawing::Font(L"Adobe Arabic", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lblTitle->ForeColor = System::Drawing::Color::White;
-			this->lblTitle->Location = System::Drawing::Point(0, 0);
-			this->lblTitle->Name = L"lblTitle";
-			this->lblTitle->Size = System::Drawing::Size(1156, 116);
-			this->lblTitle->TabIndex = 0;
-			this->lblTitle->Text = L"kmr";
-			this->lblTitle->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			this->lblTitle->Click += gcnew System::EventHandler(this, &Admin_Page::label1_Click);
-			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(90)), static_cast<System::Int32>(static_cast<System::Byte>(40)),
-				static_cast<System::Int32>(static_cast<System::Byte>(120)));
-			this->button3->FlatAppearance->BorderSize = 0;
-			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button3->Font = (gcnew System::Drawing::Font(L"Arial", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(178)));
-			this->button3->ForeColor = System::Drawing::Color::White;
-			this->button3->Location = System::Drawing::Point(800, 300);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(240, 60);
-			this->button3->TabIndex = 1;
-			this->button3->Text = L"kmr";
-			this->button3->UseVisualStyleBackColor = false;
-			this->button3->Click += gcnew System::EventHandler(this, &Admin_Page::button1_Click);
-			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(90)), static_cast<System::Int32>(static_cast<System::Byte>(40)),
-				static_cast<System::Int32>(static_cast<System::Byte>(120)));
-			this->button1->FlatAppearance->BorderSize = 0;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Arial", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(178)));
-			this->button1->ForeColor = System::Drawing::Color::White;
-			this->button1->Location = System::Drawing::Point(520, 300);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(240, 60);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"kmr";
-			this->button1->UseVisualStyleBackColor = false;
-			this->button1->Click += gcnew System::EventHandler(this, &Admin_Page::button1_Click);
-			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(90)), static_cast<System::Int32>(static_cast<System::Byte>(40)),
-				static_cast<System::Int32>(static_cast<System::Byte>(120)));
-			this->button2->FlatAppearance->BorderSize = 0;
-			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Arial", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(178)));
-			this->button2->ForeColor = System::Drawing::Color::White;
-			this->button2->Location = System::Drawing::Point(240, 300);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(240, 60);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"kmr";
-			this->button2->UseVisualStyleBackColor = false;
-			this->button2->Click += gcnew System::EventHandler(this, &Admin_Page::button1_Click);
-			this->button4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(90)), static_cast<System::Int32>(static_cast<System::Byte>(40)),
-				static_cast<System::Int32>(static_cast<System::Byte>(120)));
-			this->button4->FlatAppearance->BorderSize = 0;
-			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button4->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button4->ForeColor = System::Drawing::Color::White;
-			this->button4->Location = System::Drawing::Point(15, 20);
-			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(75, 49);
-			this->button4->TabIndex = 2;
-			this->button4->Text = L"kmr";
-			this->button4->UseVisualStyleBackColor = false;
-			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(118)), static_cast<System::Int32>(static_cast<System::Byte>(63)),
-				static_cast<System::Int32>(static_cast<System::Byte>(155)));
-			this->ClientSize = System::Drawing::Size(1264, 681);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->panel1);
-			this->MaximizeBox = false;
-			this->Name = L"Admin_Page";
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Load += gcnew System::EventHandler(this, &Admin_Page::Admin_Page_Load_1);
-			this->panel1->ResumeLayout(false);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxLogo))->EndInit();
-			this->ResumeLayout(false);
+    private:
+        void InitializeComponent(void)
+        {
+            System::ComponentModel::ComponentResourceManager^ resources =
+                gcnew System::ComponentModel::ComponentResourceManager(Admin_Page::typeid);
 
-		}
+            this->panel1 = gcnew Panel();
+            this->pictureBoxLogo = gcnew PictureBox();
+            this->lblTitle = gcnew Label();
+
+            this->btnStudents = gcnew Button();
+            this->btnDepartments = gcnew Button();
+            this->btnResult = gcnew Button();
+            this->buttonBack = gcnew Button();
+
+            this->SuspendLayout();
+            this->panel1->SuspendLayout();
+            (cli::safe_cast<ISupportInitialize^>(this->pictureBoxLogo))->BeginInit();
+
+            // ===== Header Panel =====
+            this->panel1->BackColor = Color::FromArgb(83, 39, 112);
+            this->panel1->Location = Point(96, 12);
+            this->panel1->Size = Drawing::Size(1156, 116);
+
+            this->panel1->Controls->Add(this->pictureBoxLogo);
+            this->panel1->Controls->Add(this->lblTitle);
+
+            // ===== Logo =====
+            
+            this->pictureBoxLogo->Location = Point(1035, 7);
+            this->pictureBoxLogo->Size = Drawing::Size(101, 98);
+            this->pictureBoxLogo->SizeMode = PictureBoxSizeMode::Zoom;
+
+            // ===== Title =====
+            this->lblTitle->Dock = DockStyle::Fill;
+            this->lblTitle->Font = gcnew Drawing::Font(L"Arial", 24, FontStyle::Bold);
+            this->lblTitle->ForeColor = Color::White;
+            this->lblTitle->Text = L"Admin Page";
+            this->lblTitle->TextAlign = ContentAlignment::MiddleCenter;
+
+            // ===== Buttons =====
+            SetupButton(this->btnStudents, L"Students", Point(240, 300));
+            SetupButton(this->btnDepartments, L"Departments", Point(520, 300));
+            SetupButton(this->btnResult, L"Results", Point(800, 300));
+
+            // Back button
+            this->buttonBack->Text = L"<";
+            this->buttonBack->Location = Point(15, 20);
+            this->buttonBack->Size = Drawing::Size(75, 49);
+
+            // ===== Form =====
+            this->BackColor = Color::FromArgb(118, 63, 155);
+            this->ClientSize = Drawing::Size(1264, 681);
+            this->StartPosition = FormStartPosition::CenterScreen;
+            this->MaximizeBox = false;
+
+            this->Controls->Add(this->buttonBack);
+            this->Controls->Add(this->btnStudents);
+            this->Controls->Add(this->btnDepartments);
+            this->Controls->Add(this->btnResult);
+            this->Controls->Add(this->panel1);
+
+            (cli::safe_cast<ISupportInitialize^>(this->pictureBoxLogo))->EndInit();
+            this->panel1->ResumeLayout(false);
+            this->ResumeLayout(false);
+        }
 #pragma endregion
-	private: System::Void Admin_Page_Load(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void Admin_Page_Load_1(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
-	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-};
+
+        // ===== Helper for buttons =====
+    private:
+        void SetupButton(Button^ btn, String^ text, Point location)
+        {
+            btn->Text = text;
+            btn->Location = location;
+            btn->Size = Drawing::Size(240, 60);
+            btn->BackColor = Color::FromArgb(90, 40, 120);
+            btn->FlatStyle = FlatStyle::Flat;
+            btn->FlatAppearance->BorderSize = 0;
+            btn->Font = gcnew Drawing::Font(L"Arial", 18, FontStyle::Bold);
+            btn->ForeColor = Color::White;
+        }
+    };
 }
--- CppWinFormsApp3/Main_Student.h
+++ CppWinFormsApp3/Main_Student.h
@@ -2,107 +2,138 @@
 
 namespace CppWinFormsApp3 {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Windows::Forms;
-	using namespace System::Drawing;
-	using namespace System::Drawing::Drawing2D;
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Windows::Forms;
+    using namespace System::Drawing;
+    using namespace System::Drawing::Drawing2D;
+    using namespace System::Drawing;
 
-	public ref class Main_student : public Form
-	{
-	public:
-		Main_student()
-		{
-			InitializeComponent();
-		}
+    public ref class Main_student : public Form
+    {
+    public:
+        Main_student()
+        {
+            InitializeComponent();
+        }
 
-	protected:
-		~Main_student()
-		{
-			if (components) delete components;
-		}
+    protected:
+        ~Main_student()
+        {
+            if (components)
+                delete components;
+        }
 
-	private:
-		Panel^ Headr;
-		System::ComponentModel::Container^ components;
+    private:
+        Panel^ Headr;
+        System::ComponentModel::Container^ components;
 
-	protected:
-		virtual void OnPaint(PaintEventArgs^ e) override
-		{
-			LinearGradientBrush^ brush =
-				gcnew LinearGradientBrush(
-					this->ClientRectangle,
-					Color::FromArgb(92, 15, 143),
-					Color::FromArgb(179, 179, 179),
-					LinearGradientMode::Horizontal);
+        // ================= FORM BACKGROUND GRADIENT =================
+    protected:
+        virtual void OnPaint(PaintEventArgs^ e) override
+        {
+            if (this->DesignMode)
+            {
+                Form::OnPaint(e);
+                return;
+            }
 
-			e->Graphics->FillRectangle(brush, this->ClientRectangle);
-			Form::OnPaint(e);
-		}
+            LinearGradientBrush^ brush =
+                gcnew LinearGradientBrush(
+                    this->ClientRectangle,
+                    Color::FromArgb(92, 15, 143),
+                    Color::FromArgb(179, 179, 179),
+                    LinearGradientMode::Horizontal);
 
-	private:
-		GraphicsPath^ GetRoundedRect(Rectangle rect, int radius)
-		{
-			GraphicsPath^ path = gcnew GraphicsPath();
-			int d = radius * 2;
+            e->Graphics->FillRectangle(brush, this->ClientRectangle);
+            Form::OnPaint(e);
+        }
 
-			path->AddArc(rect.X, rect.Y, d, d, 180, 90);
-			path->AddArc(rect.Right - d, rect.Y, d, d, 270, 90);
-			path->AddArc(rect.Right - d, rect.Bottom - d, d, d, 0, 90);
-			path->AddArc(rect.X, rect.Bottom - d, d, d, 90, 90);
-			path->CloseFigure();
+        // ================= ROUNDED RECT FUNCTION =================
+    private:
+        GraphicsPath^ GetRoundedRect(System::Drawing::Rectangle rect, int radius)
+        {
+            GraphicsPath^ path = gcnew GraphicsPath();
+            int d = radius * 2;
 
-			return path;
-		}
+            path->AddArc(rect.X, rect.Y, d, d, 180, 90);
+            path->AddArc(rect.Right - d, rect.Y, d, d, 270, 90);
+            path->AddArc(rect.Right - d, rect.Bottom - d, d, d, 0, 90);
+            path->AddArc(rect.X, rect.Bottom - d, d, d, 90, 90);
+            path->CloseFigure();
 
-		void InitializeComponent()
-		{
-			this->Headr = gcnew Panel();
-			this->SuspendLayout();
+            return path;
+        }
 
-			this->Headr->Height = 150;
-			this->Headr->BackColor = Color::Transparent;
-			this->Headr->Anchor = AnchorStyles::Top | AnchorStyles::Left | AnchorStyles::Right;
-			this->Headr->Paint += gcnew PaintEventHandler(this, &Main_student::Headr_Paint);
+        // ================= INITIALIZE COMPONENT =================
+    private:
+        void InitializeComponent()
+        {
+            this->components = gcnew System::ComponentModel::Container();
+            this->Headr = gcnew Panel();
 
-			this->ClientSize = Drawing::Size(1032, 588);
-			this->Controls->Add(this->Headr);
-			this->WindowState = FormWindowState::Maximized;
-			// prevent resizing
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
-			this->MaximizeBox = false;
+            this->SuspendLayout();
 
-			this->Text = L"Student Main Page";
-			this->Load += gcnew EventHandler(this, &Main_student::Main_student_Load);
+            // ===== Header Panel =====
+            this->Headr->Height = 150;
+            this->Headr->Left = 40;
+            this->Headr->Top = 30;
+            this->Headr->Width = 800;
+            this->Headr->BackColor = this->BackColor; // NOT Transparent
+            this->Headr->Anchor =
+                AnchorStyles::Top | AnchorStyles::Left | AnchorStyles::Right;
 
-			this->ResumeLayout(false);
-		}
+            this->Headr->Paint +=
+                gcnew PaintEventHandler(this, &Main_student::Headr_Paint);
 
-	private:
-		System::Void Main_student_Load(System::Object^ sender, System::EventArgs^ e)
-		{
-			int marginLeft = 40;
-			int marginRight = 40;
-			int marginTop = 30;
+            // ===== Form =====
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+            this->ClientSize = Drawing::Size(1032, 588);
+            this->Text = L"Student Main Page";
+            this->WindowState = FormWindowState::Maximized;
+            this->FormBorderStyle =
+            System::Windows::Forms::FormBorderStyle::FixedSingle;this->MaximizeBox = false;
 
-			this->Headr->Top = marginTop;
-			this->Headr->Left = marginLeft;
-			this->Headr->Width = this->ClientSize.Width - (marginLeft + marginRight);
-		}
+            this->Load +=
+                gcnew EventHandler(this, &Main_student::Main_student_Load);
 
-	private:
-		System::Void Headr_Paint(System::Object^ sender, PaintEventArgs^ e)
-		{
-			e->Graphics->SmoothingMode = SmoothingMode::AntiAlias;
+            // ===== Add Controls =====
+            this->Controls->Add(this->Headr);
 
-			Rectangle rect = Headr->ClientRectangle;
-			rect.Width -= 1;
-			rect.Height -= 1;
+            this->ResumeLayout(false);
+        }
 
-			GraphicsPath^ path = GetRoundedRect(rect, 30);
-			SolidBrush^ brush = gcnew SolidBrush(Color::FromArgb(80, 34, 92));
+        // ================= LOAD EVENT =================
+    private:
+        System::Void Main_student_Load(System::Object^ sender, System::EventArgs^ e)
+        {
+            int marginLeft = 40;
+            int marginRight = 40;
+            int marginTop = 30;
 
-			e->Graphics->FillPath(brush, path);
-		}
-	};
+            this->Headr->Top = marginTop;
+            this->Headr->Left = marginLeft;
+            this->Headr->Width =
+                this->ClientSize.Width - (marginLeft + marginRight);
+        }
+
+        // ================= HEADER PAINT =================
+    private:
+        System::Void Headr_Paint(System::Object^ sender, PaintEventArgs^ e)
+        {
+            if (this->DesignMode) return;
+
+            e->Graphics->SmoothingMode = SmoothingMode::AntiAlias;
+
+            System::Drawing::Rectangle rect = Headr->ClientRectangle;
+            rect.Width -= 1;
+            rect.Height -= 1;
+
+            GraphicsPath^ path = GetRoundedRect(rect, 30);
+            SolidBrush^ brush =
+                gcnew SolidBrush(Color::FromArgb(80, 34, 92));
+
+            e->Graphics->FillPath(brush, path);
+        }
+    };
 }
